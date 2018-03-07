08b1448c7673b2728fc746ebc1c208c8f9c1c88b
Ludvig Johansson <Ludvig@dhcp-10-22-39-149.wlan.ntnu.no>
2018-03-07 13:12:40 +0100

    init

---
+++ android/src/com/progark_/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.progark_.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.progark_.game.ProgArkGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ProgArkGame(), config);
+	}
+}
---
+++ desktop/src/com/progark_/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.progark_.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.progark_.game.ProgArkGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ProgArkGame(), config);
+	}
+}
