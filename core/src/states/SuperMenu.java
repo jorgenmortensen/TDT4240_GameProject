5409cec0d2c71346b2dd33a85ffde83bd2da03ba
Jorgen Mortensen <jorgen.bm96@gmail.com>
2018-03-09 13:30:31 +0100

    Created superMenu

---
+++ core/src/states/SuperMenu.java
@@ -0,0 +1,16 @@
+package states;
+
+/**
+ * Created by Jørgen on 09.03.2018.
+ */
+
+public abstract class SuperMenu extends State{
+
+
+    protected SuperMenu(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    
+
+}
